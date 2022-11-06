47721f1e4d63010332e87481985d8e024f51c6ed
kaustubh-s1 <shuklakaustubh84@gmail.com>
2022-11-06 07:15:15 +0530

    file changes

--- main.py
+++ main.py
@@ -3,12 +3,18 @@ import uvicorn
 from fastapi import FastAPI
 import pickle
 import numpy as np
+import pandas as pd
 from pydantic import BaseModel
 from utils import data_dict
 
 app = FastAPI()
 pickle_in = open("models/mlp_classifier.pkl", "rb")
 classifier=pickle.load(pickle_in)
+
+data = pd.read_csv("dataset/dis_sym_dataset_comb.csv").dropna(axis=1)
+
+diseases = list(set(data['label_dis']))
+diseases.sort()
 
 class Symptoms(BaseModel):
     symptoms: str
@@ -37,6 +43,35 @@ def predict_disease(symptoms: Symptoms):
      
     predictions = {
         "final_prediction": model_prediction
+    }
+    return predictions
+
+
+@app.post('/predict_prob/')
+def predictDisease_prob(symptoms):
+    symptoms = symptoms.split(",")
+
+    input_data = [0] * len(data_dict["symptom_index"])
+    for symptom in symptoms:
+        index = data_dict["symptom_index"][symptom.capitalize()]
+        input_data[index] = 1
+
+    input_data = np.array(input_data).reshape(1, -1)
+
+    prediction = classifier.predict_proba(input_data)
+
+    k = 5
+
+    topk = prediction[0].argsort()[-k:][::-1]
+    # print(topk)
+    top_diseases = {}
+    for i in topk:
+        print(diseases[i], prediction[0][i]*100)
+        if(prediction[0][i]*100):
+            top_diseases[diseases[i]] = prediction[0][i]*100
+
+    predictions = {
+        "final_prediction": top_diseases
     }
     return predictions
 
